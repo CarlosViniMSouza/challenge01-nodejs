b6b9dcb9efa5d61a1d99c21a1f8a4c72484b7a5b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-10-05 08:30:13 -0300

    challenge done!

---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "challenge done!"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/challenger01-nodejs.git
+git push -u origin main
+git status
